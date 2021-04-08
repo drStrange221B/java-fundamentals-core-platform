811a8541c7a2132b8f88e70d549d4953f82e5eb7
Rojaya Maharjan <rojaya.mh3@gmail.com>
2021-04-08 19:37:55 -0400

    String Formatting and Regular Expressions

--- src/com/emrys/core/platform/java/Main.java
+++ src/com/emrys/core/platform/java/Main.java
@@ -6,41 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        doTryCatchFinally();
-        doCloseThing();
 
-    }
 
-    public static void doTryCatchFinally(){
-        try(Reader reader = new InputStreamReader(new FileInputStream(".//Resources//test-data.txt"));
-            Writer writer = new OutputStreamWriter(new FileOutputStream(".//Resources//test-insert-data.txt"))
-        )
-        {
-            int length;
-            char[] charArray = new char[5];
-            while ((length = reader.read(charArray))>=0){
-                System.out.println(length);
-                writer.write(charArray,0,length);
-
-            }
-        }catch (FileNotFoundException e){
-            System.out.println(e.getMessage());
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private static void doCloseThing(){
-        try(MyAutoClosable myAutoClosable = new MyAutoClosable()){
-            myAutoClosable.saySomething();
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-
-            for(Throwable ex: e.getSuppressed()){
-                System.out.println(ex.getMessage());
-
-            }
-        }
     }
 
 }
